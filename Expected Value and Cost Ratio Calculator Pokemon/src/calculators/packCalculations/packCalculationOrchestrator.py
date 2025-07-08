1f37dad966b028354c33364f121de87533bbeb86
Donald Stivison Jr <donnystiv@gmail.com>
2025-07-07 19:04:35 -0700

    God pack and Demi god pack logic working for both set with and without

--- Expected Value and Cost Ratio Calculator Pokemon/src/calculators/packCalculations/packCalculationOrchestrator.py
+++ Expected Value and Cost Ratio Calculator Pokemon/src/calculators/packCalculations/packCalculationOrchestrator.py
@@ -26,7 +26,7 @@ class PackCalculationOrchestrator(PackCalculations):
         hit_probability_percentage, no_hit_probability_percentage = self.calculate_hit_probability(df)
         
         # Calculate total EV with special pack adjustments
-        total_ev, special_pack_metrics, regular_pack_adjustments = self.calculate_total_ev(ev_totals, df)
+        total_ev, regular_pack_contribution, god_pack_ev_contribution, demi_god_pack_ev_contribution = self.calculate_total_ev(ev_totals, df)
         
         # Calculate pack metrics
         pack_metrics = self.calculate_pack_metrics(total_ev, pack_price)
@@ -51,10 +51,9 @@ class PackCalculationOrchestrator(PackCalculations):
             "expected_value_check": weighted_pack_metrics['expected_value_check'],
             "variance_breakdown": weighted_pack_metrics['variance_breakdown'],
             # Special pack metrics
-            "god_pack_ev": special_pack_metrics['god_pack_ev'],
-            "demi_god_pack_ev": special_pack_metrics['demi_god_pack_ev'],
-            "regular_pack_probability": regular_pack_adjustments['regular_pack_probability'],
-            "adjusted_regular_ev": regular_pack_adjustments['adjusted_regular_ev'],
+            "regular_pack_ev_contribution": regular_pack_contribution,
+            "god_pack_ev_contribution": god_pack_ev_contribution,
+            "demi_god_pack_ev_contribution": demi_god_pack_ev_contribution,
         }
         
         summary_data = {
@@ -73,20 +72,15 @@ class PackCalculationOrchestrator(PackCalculations):
             "reverse_multiplier": self.reverse_multiplier,
             "rare_multiplier": self.rare_multiplier,
             "total_ev": total_ev,
+            "regular_pack_ev_contribution": regular_pack_contribution,
+            "god_pack_ev_contribution": god_pack_ev_contribution,
+            "demi_god_pack_ev_contribution": demi_god_pack_ev_contribution,
             "net_value": pack_metrics['net_value'],
             "roi": pack_metrics['roi'],
             "roi_percent": pack_metrics['roi_percent'],
             "no_hit_probability_percentage": no_hit_probability_percentage,
             "hit_probability_percentage": hit_probability_percentage,
             "variance_breakdown": weighted_pack_metrics['variance_components'],
-            # Special pack data
-            "god_pack_ev": special_pack_metrics['god_pack_ev'],
-            "demi_god_pack_ev": special_pack_metrics['demi_god_pack_ev'],
-            "god_pack_probability": special_pack_metrics['god_pack_probability'],
-            "demi_god_pack_probability": special_pack_metrics['demi_god_pack_probability'],
-            "regular_pack_probability": regular_pack_adjustments['regular_pack_probability'],
-            "original_regular_ev": regular_pack_adjustments['original_regular_ev'],
-            "adjusted_regular_ev": regular_pack_adjustments['adjusted_regular_ev'],
         }
         
         print("=== PACK EV CALCULATION COMPLETE ===")
