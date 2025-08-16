dd8237473401e1821b36553e07e61c7e8b64521b
Donald Stivison Jr <donnystiv@gmail.com>
2025-08-15 18:19:58 -0700

    Refactor sample_god_pack function to handle different types of 'fixed' type god packs

--- Expected Value and Cost Ratio Calculator Pokemon/src/utils/monteCarloSimUtils/specialPackLogic.py
+++ Expected Value and Cost Ratio Calculator Pokemon/src/utils/monteCarloSimUtils/specialPackLogic.py
@@ -1,13 +1,25 @@
 import numpy as np
 
-def sample_god_pack(strategy_config, df):
-    strategy = strategy_config.get("strategy", {})
+def sample_god_pack(god_cfg, df):
+    strategy = god_cfg.get("strategy", {})
     strategy_type = strategy.get("type", "fixed")
 
     if strategy_type == "fixed":
-        cards = strategy.get("cards", [])
-        card_values = df[df["Card Name"].isin(cards)]["Price ($)"]
-        return card_values.sum()
+        if "packs" in strategy:
+            packs = strategy["packs"]
+            chosen_pack = np.random.choice(packs)
+            trio_value = df[df["Card Name"].isin(chosen_pack["cards"])]["Price ($)"].sum()
+            avg_common = df[df["Rarity"] == "common"]["Price ($)"].mean()
+            avg_uncommon = df[df["Rarity"] == "uncommon"]["Price ($)"].mean()
+            total_value = trio_value + 4 * avg_common + 3 * avg_uncommon
+            # print("god pack: ", total_value)
+            return total_value
+        elif "cards" in strategy:
+            cards = strategy.get("cards", [])
+            card_values = df[df["Card Name"].isin(cards)]["Price ($)"]
+            total_value = card_values.sum()
+            # print("god pack: ", total_value)
+            return total_value
 
     elif strategy_type == "random":
         rules = strategy.get("rules", {})
@@ -20,18 +32,16 @@ def sample_god_pack(strategy_config, df):
             return total_value
 
         if isinstance(rarities, list):
-            # If 'rarities' is a single list, treat all cards as same rarity pool
             eligible = df[df["Rarity"].isin(rarities)]
             if not eligible.empty:
                 total_value += eligible["Price ($)"].sample(count, replace=True).sum()
 
         elif isinstance(rarities, dict):
-            # New strategy: multiple rarity slots defined as counts
             for rarity, qty in rarities.items():
                 pool = df[df["Rarity"].str.lower().str.strip() == rarity.lower().strip()]
                 if not pool.empty:
                     total_value += pool["Price ($)"].sample(qty, replace=True).sum()
-
+        # print("god pack: ", total_value)
         return total_value
 
     return 0.0
